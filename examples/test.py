059c6b142684d0c95903f1b68521455151650eee
Andrew Siemer <35670954+andrewsiemer@users.noreply.github.com>
2021-03-22 10:38:48 -0500

    Delete test.py

--- examples/test.py
+++
@@ -1,8 +0,0 @@
-from AesEverywhere import aes256
-
-# encryption
-encrypted = aes256.encrypt('1234567', 'E@gleM0BileP@ss')
-print(encrypted)
-
-# decryption
-print(aes256.decrypt(encrypted, 'E@gleM0BileP@ss'))
